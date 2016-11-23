363b980ea6e924d440fabac1bf34defa454d157b
13aksh <1392aksh@gmail.com>
2016-11-23 20:44:32 +0530

    hindi dictionary added

--- game/urls.py
+++ game/urls.py
@@ -2,5 +2,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^$', views.index, name='index')
+	# Game homepage
+	url(r'^$', views.index, name='index'),
+
+	# Tickets genration page
+	url(r'^(?P<noticks>[0-9]+)/$', views.tickets, name='tickets'),
 ]
